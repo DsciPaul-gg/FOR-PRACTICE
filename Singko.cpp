d79cb0db5f4759c75c134b2a8552a47687a22c49
DsciPaul-gg <paulashleydelacruz.26@gmail.com>
2023-10-02 19:41:50 +0800

    Rommel

---
+++ Singko.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    double num1, num2, sum;
+
+    cout<< "Enter First Number: ";
+    cin>> num1;
+
+    cout<< "Enter Second Number: ";
+    cin>> num2;
+
+    sum= num1 + num2;
+    cout<< "The sum of the numbers is " << sum;
+
+    return 0;
+
+
+}
